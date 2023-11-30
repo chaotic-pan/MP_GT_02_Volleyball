14d4bdc3b16132d4ec9cce85ad7064c241ee214c
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2023-11-30 13:37:09 +0100

    dancing queens

--- AnimatedSprite2D.gd
+++ AnimatedSprite2D.gd
@@ -7,27 +7,31 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	startPos(Vector2(540,350))	
-	
+	startPos(Vector2(540,350))
+	match player:
+		1: play("stand_snic")
+		2: play("stand_knokels")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO
-	if player==1:
-		if Input.is_action_pressed("P1_right"):
-			velocity.x += 1
-		if Input.is_action_pressed("P1_left"):
-			velocity.x -= 1
-		if Input.is_action_pressed("P1_jump"):
-			#TODO make character jump
-			velocity.x -= 1 # this is wrong
-	elif player==2:
-		if Input.is_action_pressed("P2_right"):
-			velocity.x += 1
-		if Input.is_action_pressed("P2_left"):
-			velocity.x -= 1
-		if Input.is_action_pressed("P2_jump"):
-			#TODO make character jump
-			velocity.x -= 1 # this is wrong
+	match player:
+		1:
+			if Input.is_action_pressed("P1_right"):
+				velocity.x += 1
+			if Input.is_action_pressed("P1_left"):
+				velocity.x -= 1
+			if Input.is_action_pressed("P1_jump"):
+				#TODO make character jump
+				velocity.x -= 1 # this is wrong
+		2:
+			if Input.is_action_pressed("P2_right"):
+				velocity.x += 1
+			if Input.is_action_pressed("P2_left"):
+				velocity.x -= 1
+			if Input.is_action_pressed("P2_jump"):
+				#TODO make character jump
+				velocity.x -= 1 # this is wrong
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
