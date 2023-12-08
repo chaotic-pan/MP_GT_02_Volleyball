2adde26c339e2aeefd49df7535d5d23d20e4f191
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2023-12-08 15:32:59 +0100

    add background animation and music

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -5,13 +5,13 @@ var P2=0
 func _ready():
 	P1=0
 	P2=0
-	$Label1.text = str(P1)
-	$Label2.text = str(P2)
+	$Sprite2D1/Label1.text = str(P1)
+	$Sprite2D2/Label2.text = str(P2)
 
 func P1Hit():
 	P2+=1
-	$Label2.text = str(P2)
+	$Sprite2D2/Label2.text = str(P2)
 
 func P2Hit():
 	P1+=1
-	$Label1.text = str(P1)
+	$Sprite2D1/Label1.text = str(P1)
